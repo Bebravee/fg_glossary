fc35ef13f11c510fd9bc92aa561a7d96e27aad8a
Bebravee <pavelbogatenko@yandex.ru>
2025-08-28 12:59:14 +0300

    header changes

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -3,14 +3,25 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import useWindowWidth from "@/hooks/useWindowWidth";
 import NavMenuIcon from "@/assets/icons/nav_menu_icon.svg";
 
 import "./header.scss";
 
 const Header = () => {
   const pathname = usePathname();
-  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  const [isMenuVisible, setIsMenuVisible] = useState<boolean>(false);
+
+  const width = useWindowWidth();
+
+  useEffect(() => {
+    if (width > 850 && isMenuVisible) {
+      setIsMenuOpen(false);
+      setTimeout(() => setIsMenuVisible(false), 150);
+    }
+  }, [width, isMenuVisible]);
 
   const links = [
     { href: "/", label: "Home" },
@@ -20,34 +31,64 @@ const Header = () => {
   ];
 
   const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-    console.log(menuOpen);
+    if (isMenuVisible) {
+      setIsMenuOpen(false);
+      setTimeout(() => setIsMenuVisible(false), 150);
+    } else {
+      setIsMenuVisible(true);
+      setIsMenuOpen(true);
+    }
   };
 
   return (
-    <nav className="Header">
-      <div className="Header-content container">
-        <Link href="/" className="Header-logo">
-          FG Glossary RU
-        </Link>
+    <div>
+      <nav className="Header">
+        <div className="Header-content container">
+          <Link href="/" className="Header-logo">
+            FG Glossary RU
+          </Link>
 
-        <div className="Header-content-links">
+          <div className="Header-content-links">
+            {links.map((link) => (
+              <Link
+                key={link.href}
+                href={link.href}
+                className={pathname === link.href ? "active" : "non-active"}
+              >
+                {link.label}
+              </Link>
+            ))}
+          </div>
+
+          <button className="Header-content-menu-btn" onClick={toggleMenu}>
+            <NavMenuIcon className="Header-content-menu-btn-icon" />
+          </button>
+        </div>
+      </nav>
+
+      {isMenuVisible && (
+        <div
+          className={`Header-content-menu-mobile ${
+            isMenuOpen ? "open" : "close"
+          }`}
+        >
           {links.map((link) => (
             <Link
               key={link.href}
               href={link.href}
-              className={pathname === link.href ? "active" : "non-active"}
+              onClick={toggleMenu}
+              className={
+                pathname === link.href
+                  ? "menu-mobile-active"
+                  : "menu-mobile-non-active"
+              }
             >
               {link.label}
             </Link>
           ))}
         </div>
-
-        <button className="Header-content-menu-btn" onClick={toggleMenu}>
-          <NavMenuIcon className="Header-content-menu-btn-icon" />
-        </button>
-      </div>
-    </nav>
+      )}
+    </div>
   );
 };
 
---
+++ src/hooks/useWindowWidth.ts
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+const useWindowWidth = () => {
+  const [width, setWidth] = useState<number>(0);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    handleResize(); // сразу при монтировании
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return width;
+};
+
+export default useWindowWidth;
