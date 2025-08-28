1d8b9662cb50796fceea907a5bf13869d8f44753
Bebravee <pavelbogatenko@yandex.ru>
2025-08-28 11:25:14 +0300

    change header styles and delete useWindowsWidth hook

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -4,14 +4,12 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
 import { useState } from "react";
-import useWindowWidth from "../../../hooks/useWindowWidth";
 import NavMenuIcon from "@/assets/icons/nav_menu_icon.svg";
 
 import "./header.scss";
 
 const Header = () => {
   const pathname = usePathname();
-  const width = useWindowWidth();
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
 
   const links = [
@@ -33,23 +31,21 @@ const Header = () => {
           FG Glossary RU
         </Link>
 
-        {width > 850 ? (
-          <div className="Header-content-links">
-            {links.map((link) => (
-              <Link
-                key={link.href}
-                href={link.href}
-                className={pathname === link.href ? "active" : "non-active"}
-              >
-                {link.label}
-              </Link>
-            ))}
-          </div>
-        ) : (
-          <button className="Header-content-menu-btn" onClick={toggleMenu}>
-            <NavMenuIcon className="Header-content-menu-btn-icon" />
-          </button>
-        )}
+        <div className="Header-content-links">
+          {links.map((link) => (
+            <Link
+              key={link.href}
+              href={link.href}
+              className={pathname === link.href ? "active" : "non-active"}
+            >
+              {link.label}
+            </Link>
+          ))}
+        </div>
+
+        <button className="Header-content-menu-btn" onClick={toggleMenu}>
+          <NavMenuIcon className="Header-content-menu-btn-icon" />
+        </button>
       </div>
     </nav>
   );
