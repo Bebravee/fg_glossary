bbec468ca3290e831e36f75e2c17949bd36d107f
Bebravee <pavelbogatenko@yandex.ru>
2025-08-28 13:08:09 +0300

    header changes

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: {
-      rules: {
-        // говорим Turbopack: svg можно обрабатывать svgr
-        "*.svg": {
-          loaders: ["@svgr/webpack"],
-          as: "*.js", // svg становится js-компонентом
-        },
+  turbopack: {
+    rules: {
+      // говорим Turbopack: svg можно обрабатывать svgr
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js", // svg становится js-компонентом
       },
-    },
+    }
   },
 };
 
