e409acf7414d503567d8f62001f1889f607b65e8
Bebravee <pavelbogatenko@yandex.ru>
2025-08-30 18:05:36 +0300

    delete strapi

--- src/app/components/terms/terms.tsx
+++ src/app/components/terms/terms.tsx
@@ -1,7 +1,3 @@
-import { useEffect, useState } from "react";
-import type Term from "@/types/termType";
-import { getTerms } from "@/lib/api";
-import { log } from "console";
 import "./terms.scss";
 
 interface TermsProps {
@@ -9,38 +5,12 @@ interface TermsProps {
 }
 
 const Terms = ({ searchInput }: TermsProps) => {
-  const [terms, setTerms] = useState<Term[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const loadTerms = async () => {
-      try {
-        setLoading(true);
-        const response = await getTerms();
-        console.log(response);
-        setTerms(response.data);
-      } catch (err) {
-        setError(err instanceof Error ? err.message : "unknown error");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    loadTerms();
-  }, []);
-
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
-
   return (
     <div className="Terms">
-      {terms.map((term) => (
-        <div className="Terms-content" key={term.id}>
-          <h1 className="Terms-content-title">{term.title}</h1>
-          <p className="Terms-content-description">{term.description}</p>
-        </div>
-      ))}
+      <div className="Terms-content">
+        <h1 className="Terms-content-title">tile</h1>
+        <p className="Terms-content-description">description</p>
+      </div>
     </div>
   );
 };
